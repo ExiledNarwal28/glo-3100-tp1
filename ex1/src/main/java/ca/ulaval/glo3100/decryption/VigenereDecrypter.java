24e6bc7c12ce9ba43138f9b37ca1613113fd27c3
Fabien Roy <fabienroy28@gmail.com>
2021-09-23 21:17:51 -0400

    unshift subtexts

--- ex1/src/main/java/ca/ulaval/glo3100/decryption/VigenereDecrypter.java
+++ ex1/src/main/java/ca/ulaval/glo3100/decryption/VigenereDecrypter.java
@@ -4,16 +4,20 @@ import ca.ulaval.glo3100.console.Logger;
 import ca.ulaval.glo3100.utils.CharacterOccurrenceUtils;
 import ca.ulaval.glo3100.utils.ShiftedTextUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class VigenereDecrypter {
 
     private static final char ENGLISH_MOST_FREQUENT_LETTER = 'E';
+    private static final int FIRST_POSSIBLE_LETTER_POSITION = 'A';
+    private static final int LAST_POSSIBLE_LETTER_POSITION = 'Z';
 
     public static String decrypt(String cypherText, int keyLength) {
         Logger.logDebug("Decrypting cypher text");
 
         List<String> subtexts = ShiftedTextUtils.getSubtexts(cypherText, keyLength);
+        List<String> unshiftedSubtexts = new ArrayList<>();
 
         for (String subtext : subtexts) {
             Logger.logDebug(String.format("Calculating most frequent character for subtext : %s", subtext));
@@ -24,10 +28,32 @@ public class VigenereDecrypter {
 
             Logger.logDebug(String.format("--> Most frequent character : %s (shift from '%s' : %d)", mostFrequentCharacter, ENGLISH_MOST_FREQUENT_LETTER, shift));
 
-            // TODO : Un-shift each subtext
-            // TODO : Assemble back plain text
+            String unShiftedSubtext = unShiftText(subtext, shift);
+
+            Logger.logDebug(String.format("Un-shifted subtext : %s", unShiftedSubtext));
         }
 
+        // TODO : Assemble back plain text
+
         return "";
+    }
+
+    private static String unShiftText(String text, int shift) {
+        StringBuilder unShiftedText = new StringBuilder();
+        for (char character : text.toCharArray()) {
+            int characterPosition = character;
+            int unShiftedCharacterPosition = characterPosition - shift;
+
+            if (unShiftedCharacterPosition < FIRST_POSSIBLE_LETTER_POSITION) {
+                unShiftedCharacterPosition = (LAST_POSSIBLE_LETTER_POSITION + 1) - (shift - (characterPosition - FIRST_POSSIBLE_LETTER_POSITION));
+            } else if (unShiftedCharacterPosition > LAST_POSSIBLE_LETTER_POSITION) {
+                unShiftedCharacterPosition = FIRST_POSSIBLE_LETTER_POSITION + (shift - (LAST_POSSIBLE_LETTER_POSITION - characterPosition));
+            }
+
+            char unShifterCharacter = (char)(unShiftedCharacterPosition);
+            unShiftedText.append(unShifterCharacter);
+        }
+
+        return unShiftedText.toString();
     }
 }
