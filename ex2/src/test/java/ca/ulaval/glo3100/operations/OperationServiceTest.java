2269b9a85d44f0dff28013ea61e232700f5bd897
Fabien Roy <fabienroy28@gmail.com>
2021-10-04 18:26:59 -0400

    cleanup test

--- ex2/src/test/java/ca/ulaval/glo3100/operations/OperationServiceTest.java
+++ ex2/src/test/java/ca/ulaval/glo3100/operations/OperationServiceTest.java
@@ -12,7 +12,9 @@ public class OperationServiceTest {
     @Test
     public void givenEcbDecryption_whenExecuting_thenDecrypt() {
         String expectedResult = "01110001 01100001 00100000";
-        Args args = new Args("111011101111111010111111", "10011111", Operation.DECRYPT, null, Mode.ECB, 0);
+        String message = "111011101111111010111111";
+        String key = "10011111";
+        Args args = new Args(message, key, Operation.DECRYPT, null, Mode.ECB, 0);
 
         String result = OperationService.execute(args);
 
