499db496ddd7f606b886bae8123b9fff09abc953
Fabien Roy <fabienroy28@gmail.com>
2021-10-04 22:07:33 -0400

    add CBC decryption

--- ex2/src/main/java/ca/ulaval/glo3100/operations/OperationService.java
+++ ex2/src/main/java/ca/ulaval/glo3100/operations/OperationService.java
@@ -15,6 +15,7 @@ import static ca.ulaval.glo3100.utils.StringUtils.getSubstrings;
 public class OperationService {
 
     private static final int SUBSTRING_LENGTH_FOR_ECB = 8;
+    private static final int SUBSTRING_LENGTH_FOR_CBC = 8;
     // TODO : Maybe we should move XOR to EncryptionUtils
     private static final Encryption<Long> XOR = ((firstBytes, secondBytes) -> firstBytes ^ secondBytes);
 
@@ -59,27 +60,52 @@ public class OperationService {
     private static String cbc(String message, String key, String iv, Operation operation) {
         switch (operation) {
             case ENCRYPT:
-                List<String> substrings = getSubstrings(message, SUBSTRING_LENGTH_FOR_ECB);
-                List<Long> substringsBytes = getBytes(substrings);
-                long keyBytes = getByte(key);
-
-                List<Long> encryptedBytes = new ArrayList<>();
-                encryptedBytes.add(getByte(iv));
-
-                for (Long substringsByte : substringsBytes) {
-                    long lastEncryptedBytes = encryptedBytes.get(encryptedBytes.size() - 1);
-                    long partiallyEncryptedBytes = applyEncryption(substringsByte, lastEncryptedBytes, XOR);
-                    encryptedBytes.add(applyEncryption(partiallyEncryptedBytes, keyBytes, XOR));
-                }
-                
-                List<String> encryptedMessageSubstrings = getTexts(encryptedBytes);
-
-                return concatStrings(encryptedMessageSubstrings);
+                return encryptCbc(message, key, iv);
             default:
             case DECRYPT:
-                // TODO : Decryption for CBC
-                return "";
+                return decryptCbc(message, key);
         }
+    }
+
+    private static String encryptCbc(String message, String key, String iv) {
+        List<String> substrings = getSubstrings(message, SUBSTRING_LENGTH_FOR_CBC);
+        List<Long> substringsBytes = getBytes(substrings);
+        long keyBytes = getByte(key);
+
+        List<Long> encryptedBytes = new ArrayList<>();
+        encryptedBytes.add(getByte(iv));
+
+        if (substrings.size() > 1) {
+            for (Long substringsByte : substringsBytes) {
+                long lastEncryptedBytes = encryptedBytes.get(encryptedBytes.size() - 1);
+                long partiallyEncryptedBytes = applyEncryption(substringsByte, lastEncryptedBytes, XOR);
+                encryptedBytes.add(applyEncryption(partiallyEncryptedBytes, keyBytes, XOR));
+            }
+        }
+
+        List<String> encryptedMessageSubstrings = getTexts(encryptedBytes);
+
+        return concatStrings(encryptedMessageSubstrings);
+    }
+
+    // TODO : See if encrypt and decrypt can be partially merged
+    private static String decryptCbc(String message, String key) {
+        List<String> substrings = getSubstrings(message, SUBSTRING_LENGTH_FOR_CBC);
+        List<Long> substringsBytes = getBytes(substrings);
+        long keyBytes = getByte(key);
+
+        // TODO : If substring.size < 1
+
+        List<Long> decryptedBytes = new ArrayList<>();
+        for (int i = 1; i < substringsBytes.size(); i++) {
+            long partiallyDecryptedBytes = applyEncryption(substringsBytes.get(i), keyBytes, XOR);
+            long decryptedSubstringBytes = applyEncryption(substringsBytes.get(i - 1), partiallyDecryptedBytes, XOR);
+            decryptedBytes.add(decryptedSubstringBytes);
+        }
+
+        List<String> decryptedMessageSubstrings = getTexts(decryptedBytes);
+
+        return concatStrings(decryptedMessageSubstrings);
     }
 
     // TODO : Complete CFB
